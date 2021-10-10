e1e24b0e501684e3a232c0c969e23622de86ed2a
rodrigobbrozinga <rodrigobbrozinga@gmail.com>
2021-10-10 18:35:26 -0300

    Layout desafio 1

--- src/pages/Desafio1/index.jsx
+++ src/pages/Desafio1/index.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import { Card, Form, Button } from 'react-bootstrap';
 
 
 const Desafio1 = () => {
     return (
         <>
-            <h3>Conteúdo desafio 1</h3>
-            <p>Texto</p>           
+            <Card border="secondary" bg="light" className="w-50 m-auto mt-5 mb-5">
+                <Card.Body>
+                    <Card.Title className="text-center">Números palíndromos</Card.Title>
+                    <div className="formulario-desafio1">
+                        <Form /* onSubmit={submitHandler} */>
+                            <h4>Digite um intervalo</h4>
+                            <Form.Group className="mb-3" controlId="nome">
+                                <Form.Label>Início</Form.Label>
+                                <input type="number" name="inicio" className="form-control"
+                                    placeholder="Insira o início do intervalo" required />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="CEP">
+                                <Form.Label>Fim</Form.Label>
+                                <input type="number" name="fim" placeholder="Insira o fim do intervalo"
+                                    className="form-control" required />
+                            </Form.Group>
+
+                            <Button variant="primary" type="submit">Resultado</Button>
+                        </Form>
+                    </div>
+                    <div className="resultado-desafio1">
+                        <Form.Group className="mb-3 mt-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>Números palíndromos dentro do intervalo:</Form.Label>
+                            <Form.Control as="textarea" rows={3} readOnly />
+                        </Form.Group>
+                    </div>
+                </Card.Body>
+            </Card>
         </>
     );
 }
