ee14048f95e993bc8531c9f4778b49b15cf73d3c
rodrigobbrozinga <rodrigobbrozinga@gmail.com>
2021-10-10 11:58:42 -0300

    Cabeçalho e Rodapé

---
+++ src/components/cabecalho.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+
+
+function Cabecalho() {
+    return (
+        <Navbar bg="light" expand="lg" className="fixed-top">
+            <Container>
+                <Navbar.Brand href="#home">Desafio FullStack</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#">Desafio 1</Nav.Link>
+                        <Nav.Link href="#">Desafio 2</Nav.Link>
+                        <Nav.Link href="#">Desafio 3</Nav.Link>
+                        <Nav.Link href="#">Desafio 4</Nav.Link>                                                
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Cabecalho
---
+++ src/components/rodape.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+function Rodape() {
+    return (
+        <>
+            <footer className="footer mt-auto py-3 bg-light text-center">
+                <div className="container">
+                    <p>App desenvolvido por
+                        <a className="ms-1" href="https://www.linkedin.com/in/rodrigo-botelho-brozinga-a35725121/" target="_blank" rel="noreferrer">
+                            Rodrigo Botelho</a>
+                    </p>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Rodape
